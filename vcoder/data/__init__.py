c2ebafa90beca342efe0aee19f9dfe427047c85c
wachub <job.jjq@gmail.com>
2026-03-05 15:00:46 +0530

    fix: lazy-load data modules to avoid optional deps

--- vcoder/data/__init__.py
+++ vcoder/data/__init__.py
@@ -1,4 +1,19 @@
-from vcoder.data.websight import load_websight_dataset
-from vcoder.data.synth_html import load_synthetic_sft_dataset
+"""Data loaders with lazy imports to avoid optional dependency failures."""
+
+from __future__ import annotations
+
+from typing import Any
 
 __all__ = ["load_websight_dataset", "load_synthetic_sft_dataset"]
+
+
+def load_websight_dataset(*args: Any, **kwargs: Any) -> Any:
+    from vcoder.data.websight import load_websight_dataset as _load
+
+    return _load(*args, **kwargs)
+
+
+def load_synthetic_sft_dataset(*args: Any, **kwargs: Any) -> Any:
+    from vcoder.data.synth_html import load_synthetic_sft_dataset as _load
+
+    return _load(*args, **kwargs)
